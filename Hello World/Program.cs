74040a9e36ff955e6b909447ecc877dca12107fc
kjostar5463 <58179651+kjostar5463@users.noreply.github.com>
2021-03-08 09:52:54 +0900

    Add files via upload

---
+++ Hello World/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("2017848028 김준우");
+        }
+    }
+}
