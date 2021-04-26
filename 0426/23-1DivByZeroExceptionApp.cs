2117b07d7a3df2028e035f1bf3fe430dbac852d8
kjostar5463 <58179651+kjostar5463@users.noreply.github.com>
2021-04-26 18:47:47 +0900

    Add files via upload

---
+++ 0426/23-1DivByZeroExceptionApp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DivByZeroExceptionApp
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int i = 1, j = 0, k;
+            k = i / j;
+        }
+    }
+}
---
+++ 0426/23-2IndexOutOfRangeApp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IndexOutOfRangeApp
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int[] vector = { 1, 2, 3 };
+            vector[4] = 4;
+        }
+    }
+}
